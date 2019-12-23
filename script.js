9c5f57c5c6b2745051f98dd2271d3f190a02443e
Mad King <58307391+mrstrangeF@users.noreply.github.com>
2019-12-23 17:15:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+menu.onclick = function mtFunction() {
+	var x = document.getElementById('myTopnav');
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
