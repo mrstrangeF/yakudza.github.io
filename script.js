96272b90710983db1d8ff514cd5822d6672f603e
Mad King <58307391+mrstrangeF@users.noreply.github.com>
2020-01-01 15:17:38 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function mtFunction() {
-	var x = document.getElementById('myTopnav');
-
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
-}
